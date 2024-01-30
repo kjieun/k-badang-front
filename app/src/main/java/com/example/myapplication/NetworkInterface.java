c6e370cb09ed3b163b8b37e3567d9be60b432d8c
kjieun <rlawldms9258@naver.com>
2024-01-30 20:56:56 +0900

    Update NetworkInterface.java
    data clear

--- app/src/main/java/com/example/myapplication/NetworkInterface.java
+++ app/src/main/java/com/example/myapplication/NetworkInterface.java
@@ -10,20 +10,20 @@ import retrofit2.http.Query;
 public interface NetworkInterface {
     // @GET( EndPoint-자원위치(URI) )
     @GET("/default/test2")
-    @Headers({"x-api-key: UqORjPGp5uacblhwUdL117r05fzlLa9B7TAU9tNi"})
+    @Headers({""})
     Call<DataModel3> test_2(
             @Query("id") String id,
             @Query("type") String type
     );
     @GET("/default/test2")
-    @Headers({"x-api-key: UqORjPGp5uacblhwUdL117r05fzlLa9B7TAU9tNi"})
+    @Headers({""})
     Call<List<DataModelNews>> test_3(
             @Query("id") String id,
             @Query("type") String type
     );
 
     @GET("/default/test2")
-    @Headers({"x-api-key: UqORjPGp5uacblhwUdL117r05fzlLa9B7TAU9tNi"})
+    @Headers({""})
     Call<DataModelNews> test_4(
             @Query("id") String id,
             @Query("type") String type
